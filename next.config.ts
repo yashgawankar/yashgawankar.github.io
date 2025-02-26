eac3a8d87ad8794ce2ec3e764bb24b3aeec15868
Yash Gawankar <yash.gawankar@bankbuddy.ai>
2025-02-25 16:20:25 -0800

    'Deploy'

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/",
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export", // Export as static files for GitHub Pages
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next.js image optimization (GitHub Pages doesn't support it)
   },
+  basePath: "/yashgawankar.github.io", // Set your repo name as the base path
+  assetPrefix: "/yashgawankar.github.io/", // Ensure correct asset paths
 };
 
-module.exports = nextConfig;
+export default nextConfig;
