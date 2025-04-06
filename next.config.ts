30ea8a9afb29b6119f84ea561146babb0ecf581d
yashgawankar <yashgawankar@gmail.com>
2025-04-06 16:28:46 -0700

    Initial deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
