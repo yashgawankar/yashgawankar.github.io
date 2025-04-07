f24e8aef3022bc471a18c8da9613d7aab9fede66
yashgawankar <yashgawankar@gmail.com>
2025-04-06 22:57:35 -0700

    Tailwind

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+// next.config.ts
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-};
+  basePath: '/yashgawankar.github.io', // ✅ this line is crucial!
+  assetPrefix: '/yashgawankar.github.io/', // ✅ needed for Tailwind + fonts + images
+}
 
-module.exports = nextConfig;
+export default nextConfig
